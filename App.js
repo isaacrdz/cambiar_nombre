809c680e82a6035837a9414fb4945fe16f3b9f00
isaacrdz <zack_cpp@hotmail.com>
2021-06-29 09:31:06 -0500

    s

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+
+import React, { useState, useEffect, useRef } from "react";
 import Routes from "./src/Routes";
 
 //UI Kitten
@@ -15,9 +18,21 @@ import SubstatusState from "./src/contexts/substatus/SubstatusState";
 import StatusState from "./src/contexts/status/StatusState";
 import CommentState from "./src/contexts/comment/CommentState";
 
-import Toast from 'react-native-toast-message';
+import Toast from "react-native-toast-message";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const App = () => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
   const [theme, setTheme] = React.useState("light");
 
   const toggleTheme = () => {
@@ -25,29 +40,115 @@ const App = () => {
     setTheme(nextTheme);
   };
 
+  useEffect(() => {
+    const enviarPush = async () => {
+      await sendPushNotification(expoPushToken);
+    };
+
+    enviarPush();
+  }, []);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <>
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <IconRegistry icons={FeatherIconsPack} />
-      <ApplicationProvider {...eva} theme={eva[theme]}>
-        <AuthState>
-          <LeadState>
-            <ActivityState>
-              <StatusState>
-                <SubstatusState>
-                  <CommentState>
-                    <Routes />
-                    <Toast ref={(ref) => Toast.setRef(ref)} />
-                  </CommentState>
-                </SubstatusState>
-              </StatusState>
-            </ActivityState>
-          </LeadState>
-        </AuthState>
-      </ApplicationProvider>
-    </ThemeContext.Provider>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <IconRegistry icons={FeatherIconsPack} />
+        <ApplicationProvider {...eva} theme={eva[theme]}>
+          <AuthState>
+            <LeadState>
+              <ActivityState>
+                <StatusState>
+                  <SubstatusState>
+                    <CommentState>
+                      <Routes />
+                      <Toast ref={(ref) => Toast.setRef(ref)} />
+                    </CommentState>
+                  </SubstatusState>
+                </StatusState>
+              </ActivityState>
+            </LeadState>
+          </AuthState>
+        </ApplicationProvider>
+      </ThemeContext.Provider>
     </>
   );
+
+  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/notifications
+  async function sendPushNotification(expoPushToken) {
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "Original Title",
+      body: "And here is the body!",
+      data: { someData: "goes here" },
+    };
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    return token;
+  }
 };
 
 export default () => {
