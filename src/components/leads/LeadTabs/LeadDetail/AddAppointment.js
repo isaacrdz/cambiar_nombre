b840d0ecb9ce1df0f20cc35acf80a0bde7d9f052
MadMofin <Arthurjaziel1@live.com>
2021-07-06 17:49:23 -0500

    s

--- src/components/leads/LeadTabs/LeadDetail/AddAppointment.js
+++ src/components/leads/LeadTabs/LeadDetail/AddAppointment.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { StyleSheet, SafeAreaView, ScrollView, View } from "react-native";
+import Toast from "react-native-toast-message";
 import HeaderTitle from "../../../header/HeaderTitle";
+import useAppointment from '../../../../hooks/useAppointment';
+import moment from 'moment'
 import {
   Layout,
   Divider,
@@ -14,14 +17,63 @@ import {
   Input,
 } from "@ui-kitten/components";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import useLead from "../../../../hooks/useLead";
 
-const AddAppointment = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
+const AddAppointment = ({ navigation }) => {
+  const { createAppointment } = useAppointment();
+  const { lead, updateLead } = useLead(); 
+  const [date, setDate] = useState(new Date());
+  const [selectedAction, setSelectedAction] = useState(new IndexPath(0));
+  const actions = ["information", "documentation", "driving test"];
+  const times = ["1 Hora", "2 Horas"];
+  const [time, setTime] = useState({row: 0});
+  const displayValue = actions[selectedAction.row];
+  const displayValueTime = times[time.row];
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
   const [text, setText] = useState("");
+  const [values, setValues] = useState({
+    title: '',
+    description: '',
+  });
 
-  const actions = ["information", "documentation", "driving test"];
+  const handleSubmit = async() =>{
+    if(values.description === '' || values.title === ''){
+      return Toast.show({
+        text1: "Fill all the fields",
+        type: "error",
+        position: "bottom",
+      });
+    }
+    let endDate = moment(date).add((time.row + 1), 'hours');
+    await createAppointment({
+      ...values, 
+      startDate: date, 
+      endDate, 
+      action: displayValue, 
+      substatus: "605bd6b0bed49524ae40f885", 
+      store: lead.store._id, 
+      user: lead.agent._id, 
+      lead: lead._id,
+      email: lead.email,
+      customer: lead.name,
+    });
+
+    let updateLeadBody = {
+      status: '604f80222b372e0cb11966dc', 
+      substatus: '605bd6b0bed49524ae40f885', 
+      isContacted: true, 
+    }
+
+    if(!lead.firstTask){
+      updateLeadBody.firstTask = new Date()
+    }
+
+    await updateLead(updateLeadBody, lead._id);
+    navigation.navigate("LeadTabs");
+
+  }
+
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -60,9 +112,9 @@ const AddAppointment = () => {
             <Input
               placeholder="Multiline"
               style={{ minWidth: 400 }}
-              value={text}
+              value={values.title}
               onChangeText={(string) => {
-                setText(string);
+                setValues({...values, title: string});
               }}
             />
           </Layout>
@@ -91,9 +143,9 @@ const AddAppointment = () => {
               placeholder="Multiline"
               textStyle={{ minHeight: 64 }}
               style={{ minWidth: 400 }}
-              value={text}
+              value={values.description}
               onChangeText={(string) => {
-                setText(string);
+                setValues({...values, description: string});
               }}
             />
           </Layout>
@@ -103,9 +155,14 @@ const AddAppointment = () => {
           <Text style={styles.text} category="s1" style={{ marginBottom: 20 }}>
             3. Choose an Action
           </Text>
-          <Select size="large" style={{ marginBottom: 10 }} value="Selecciona">
+          <Select size="large" 
+            style={{ marginBottom: 10 }} 
+            value="Selecciona" 
+            selectedIndex={selectedAction} 
+            value={displayValue}
+            onSelect={(index) => setSelectedAction(index)}>
             {actions.map((action, i) => (
-              <SelectItem title={action} key={i} />
+              <SelectItem title={action} key={i}  />
             ))}
           </Select>
         </Layout>
@@ -127,13 +184,14 @@ const AddAppointment = () => {
           <Text style={styles.text} category="s1" style={{ marginBottom: 20 }}>
             4. Pick up Time
           </Text>
-          <Select size="large" style={{ marginBottom: 10 }} value="Selecciona">
-            <SelectItem title="1 Hora" />
-            <SelectItem title="2 Horas" />
+          <Select size="large" style={{ marginBottom: 10 }} onSelect={(index) => setTime(index)} value="Selecciona" value={displayValueTime}>
+            {times.map((action, i) => (
+              <SelectItem title={action} key={i}  />
+            ))}
           </Select>
         </Layout>
         <Layout style={{ paddingHorizontal: 15, paddingVertical: 10 }}>
-          <Button style={styles.button}>Create Appointment</Button>
+          <Button style={styles.button} onPress={handleSubmit}>Create Appointment</Button>
         </Layout>
       </ScrollView>
     </SafeAreaView>
