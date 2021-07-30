e80364b22fad75a1d889abf1bed17fc29f8a4240
isaacrdz <zack_cpp@hotmail.com>
2021-07-29 23:12:35 -0500

    S

--- src/api/api.js
+++ src/api/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 let baseURL;
 
 if (process.env.NODE_ENV === "production") {
-  baseURL = "http://localhost:500/api/v1";
+  baseURL = "http://localhost:5000/api/v1";
 } else {
-  baseURL = "http://localhost:500/api/v1";
+  baseURL = "http://localhost:5000/api/v1";
 }
 
 export default axios.create({
