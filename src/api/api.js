e813830c26bb8ba0860f44b207305dd2bcdc7692
MadMofin <Arthurjaziel1@live.com>
2021-09-07 13:53:24 -0500

    s

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ let baseURL;
 if (process.env.NODE_ENV === "production") {
   baseURL = "https://dealerproxapi.com/api/v1";
 } else {
-  baseURL = "https://8523-2806-2f0-4041-fd66-18b8-9cd6-f2ec-3a07.ngrok.io/api/v1";
+  baseURL = "https://dealerproxapi.com/api/v1";
 }
 
 export default axios.create({
