70b8a8b2a8513d9e8318882569a8b4f6293e42df
liazcortez <liazcortez@gmail.com>
2021-10-13 14:44:52 -0500

    top users update

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ let baseURL;
 if (process.env.NODE_ENV === "production") {
   baseURL = "https://dealerproxapi.com/api/v1";
 } else {
-  baseURL = "https://dealerproxapi.com/api/v1";
+  baseURL = "http://192.168.3.3:5000/api/v1";
 }
 
 export default axios.create({
