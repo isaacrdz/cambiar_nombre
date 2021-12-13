c63a7369909756f15f234833a4598458bd467d55
MadMofin <Arthurjaziel1@live.com>
2021-12-13 17:41:14 -0600

    s

--- src/api/api.js
+++ src/api/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 let baseURL;
 
 if (process.env.NODE_ENV === "production") {
-  baseURL = "https://6b18-2806-2f0-4060-2282-55d7-c7a8-f233-778d.ngrok.io/api/v1";
+  baseURL = "https://dealerproxapi.com/api/v1";
 } else {
-  baseURL = "https://6b18-2806-2f0-4060-2282-55d7-c7a8-f233-778d.ngrok.io/api/v1";
+  baseURL = "https://dealerproxapi.com/api/v1";
 
   // baseURL="https://ce95-2806-2f0-4060-2282-9532-d3a-630a-ab36.ngrok.io/api/v1"
   //  baseURL="http://192.168.3.3:5000/api/v1"
