3ab908c941563cb6c6f4598310a8ad953ebbd3b3
MadMofin <Arthurjaziel1@live.com>
2022-10-15 12:53:14 -0500

    s

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ let baseURL;
 if (process.env.NODE_ENV === "production") {
   baseURL = "https://dealerproxapi.com/api/v1";
 } else {
-  baseURL = "https://b99a-2806-2f0-4040-ff5b-e44f-dddc-e7f4-4230.ngrok.io/api/v1";
+  baseURL = "https://dealerproxapi.com/api/v1";
 }
 
 export default axios.create({
--- src/screens/Task/Task.js
+++ src/screens/Task/Task.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Agenda } from "react-native-calendars";
 import TaskItem from "../../components/task/TaskItem";
 import moment from "moment";
@@ -8,13 +8,29 @@ import useAuth from "../../hooks/useAuth";
 import { useFocusEffect } from "@react-navigation/native";
 import { getMultiStoresIds } from "../../utils/storesUser";
 import { isAdmin, isRockstar, isSuper, isUser } from "../../utils/Authroles";
+import { StyleSheet } from 'react-native'
+import useUser from "../../hooks/useUser";
+import useStore from "../../hooks/useStore";
+import { IndexPath, Layout, Select, SelectItem } from "@ui-kitten/components";
+import { Text } from "react-native-paper";
+import { CapitalizeNames } from "../../utils/Capitalize";
 
 const Appointment = () => {
   const [items, setItems] = React.useState({});
   const [marked, setMarked] = React.useState({});
   const [refreshing, setRefreshing] = React.useState(false);
-  const { getCommentsByUser, getCommentsByStore, getComments, comments, loading } = useComment();
+  const { getCommentsByUser, getCommentsByStore, clearState, comments, loading } = useComment();
   const { user } = useAuth();
+  const { getStoresByGroup, stores } = useStore()
+  const { agents, getAgents } = useUser();
+
+  const [tiendas, setTiendas] = useState([""]);
+  const [storeSelect, setStoreSelect] = useState(new IndexPath(0));
+  const displayStore = tiendas[storeSelect.row];
+
+  const [agentes, setAgentes] = useState([""]);
+  const [agenteSelect, setAgenteSelect] = useState(new IndexPath(0));
+  const displayAgente = agentes[agenteSelect.row];
 
   React.useEffect(() => {
     let mark = [];
@@ -73,10 +89,26 @@ const Appointment = () => {
       }else if(user && user.tier && isAdmin(user.tier._id) && user.stores){
         getCommentsByStore(`&store[in]=${getMultiStoresIds(user.stores)}`)
       }else if(user && user.tier && (isRockstar(user.tier._id) || isSuper(user.tier._id))){
-        getComments();
+        getStoresByGroup('61003180d1cd3b1299a82fd0')
       }
     }, [user])
   );
+
+  React.useEffect(() => {
+
+    if(stores && stores.length > 0){
+      let aux = stores.map((item) => CapitalizeNames(item.make.name + " " + item.name));
+      setTiendas(['Selecciona una agencia', ...aux]);
+    }
+  },[stores])
+
+  React.useEffect(() => {
+
+    if(agents && agents.length > 0){
+      let aux = agents.map((item) => CapitalizeNames(item.name));
+      setAgentes(['Selecciona un agente', ...aux]);
+    }
+  },[agents])
 
   const renderEmptyDate = () => {
     return <EmpyDate />;
@@ -86,23 +118,81 @@ const Appointment = () => {
     return <TaskItem item={item} />;
   };
 
+  React.useEffect(() => {
+    if(storeSelect.row !== 0){
+      getAgents(`&stores[in]=${stores[storeSelect.row - 1]._id}`);
+    }
+  }, [storeSelect])
+
   const handleRefresh = () => {
     if (user && user.tier && isUser(user.tier._id)) {
       getCommentsByUser(user._id)
     }else if(user && user.tier && isAdmin(user.tier._id) && user.stores){
       getCommentsByStore(`&store[in]=${getMultiStoresIds(user.stores)}`)
-    }else if(user && user.tier && (isRockstar(user.tier._id) || isSuper(user.tier._id))){
-      getComments();
     }
   };
 
+  React.useEffect(() => {
+    if(stores && stores.length > 0){
+      let string = storeSelect.row !== 0 ? `&store[in]=${stores[storeSelect.row - 1]._id}` : '';
+      if(agenteSelect.row !== 0) string += `&user=${agents[agenteSelect.row - 1]._id}`
+      if(string !== '') getCommentsByStore(string)
+      else clearState()
+    }
+  }, [storeSelect, agenteSelect])
+
   return (
+    <Fragment>
+      {
+        user && user.tier &&
+        (
+          (
+            isRockstar(user.tier._id) || isSuper(user.tier._id)
+          ) ||
+          (
+            (isAdmin(user.tier._id) || isUser(user.tier._did)) && user.stores && user.stores.length > 1 
+          ) 
+        ) &&
+       <Layout style={{ paddingHorizontal: 15, paddingVertical: 5 }} level="1">
+        <Text style={{...styles.text, marginBottom: 10}} category="s1">
+          Agencia
+        </Text>
+        <Select
+          size="large"
+          onSelect={(index) => setStoreSelect(index)}
+          placeholder='Selecciona una agencia'
+          value={displayStore}
+          >
+          {
+            tiendas.map((item) => <SelectItem title={CapitalizeNames(item)} key={item} />)
+          }
+        </Select>
+      </Layout>
+      }
+      {
+        user && user.tier && !isUser(user.tier._id) &&
+        <Layout style={{ paddingHorizontal: 15, paddingVertical: 5 }} level="1">
+          <Text style={{...styles.text, marginBottom: 10}} category="s1">
+            Agente
+          </Text>
+          <Select
+            size="large"
+            onSelect={(index) => setAgenteSelect(index)}
+            placeholder='Selecciona una agente'
+            value={displayAgente}
+            >
+            {
+              agentes.map((item) => <SelectItem title={CapitalizeNames(item)} key={item} />)
+            }
+          </Select>
+        </Layout>
+      }
     <Agenda
       items={items}
       renderItem={renderItem}
       renderEmptyData={renderEmptyDate}
       markedDates={marked}
-        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
       onRefresh={() => handleRefresh()}
       refreshing={loading}
       // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
@@ -110,8 +200,22 @@ const Appointment = () => {
       // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
       // maxDate={"2025-05-30"}
       showClosingKnob={true}
-    />
+      />
+    </Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    margin: 2,
+  },
+  button: {
+    marginTop: 20,
+  },
+});
 
 export default Appointment;
